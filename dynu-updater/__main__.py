d010c7181e9161e0b3ac56fe0d9223627e561efe
Alexander Angelidis <alex.m.angelidis@gmail.com>
2024-06-21 16:01:54 +0100

    Reformat to classify nicely

--- dynu-updater/__main__.py
+++ dynu-updater/__main__.py
@@ -4,113 +4,102 @@ import requests
 import threading
 import customtkinter as ctk
 from tkinter import messagebox
-
 from .countdownthread import CountdownThread
 
-client_id = None
-api_key = None
-timer_thread: CountdownThread = None
-url = "https://api.dynu.com/v2/oauth2/token"
-
-with open("secret.json") as f:
-    secret_data = json.load(f)
-    client_id = secret_data["client_id"]
-    api_key = secret_data["client_secret"]
-
-
-def on_window_close():
-    if timer_thread is not None:
-        timer_thread.kill()
-    window.destroy()
-    exit(0)
-
-
-def fetch_data():
-    global timer_thread
-    response = requests.get(url, auth=(client_id, api_key))
-    response_json = response.json()
-
-    # Check authentication
-    match response.status_code:
-        case 401:
+class DynuAPIUpdater:
+    def __init__(self):
+        self.client_id = None
+        self.api_key = None
+        self.timer_thread = None
+        self.url = "https://api.dynu.com/v2/oauth2/token"
+        
+        with open("secret.json") as f:
+            secret_data = json.load(f)
+            self.client_id = secret_data["client_id"]
+            self.api_key = secret_data["client_secret"]
+        
+        self.create_gui()
+    
+    def create_gui(self):
+        self.window = ctk.CTk()
+        self.window.title("Dynu API Updater")
+        self.window.iconbitmap("favicon.ico")
+        self.window.protocol("WM_DELETE_WINDOW", self.on_window_close)
+        
+        self.window.minsize(400, 700)
+        
+        self.window.grid_columnconfigure((0, 1), weight=1)
+        self.window.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=0)
+        self.window.grid_rowconfigure(7, weight=30)
+        
+        self.client_id_label = ctk.CTkLabel(self.window, text="Client ID:")
+        self.client_id_label.grid(row=0, column=0)
+        self.client_id_entry = ctk.CTkEntry(self.window)
+        self.client_id_entry.insert(ctk.END, self.client_id)
+        self.client_id_entry.grid(row=1, column=0)
+        
+        self.api_key_label = ctk.CTkLabel(self.window, text="API Key:")
+        self.api_key_label.grid(row=2, column=0)
+        self.api_key_entry = ctk.CTkEntry(self.window)
+        self.api_key_entry.insert(ctk.END, self.api_key)
+        self.api_key_entry.grid(row=3, column=0)
+        
+        self.timer_label = ctk.CTkLabel(self.window, text="Timer (seconds):")
+        self.timer_label.grid(row=1, column=1)
+        
+        self.timer_time_label = ctk.CTkLabel(self.window, text="0")
+        self.timer_time_label.grid(row=2, column=1)
+        
+        self.request_button = ctk.CTkButton(self.window, text="Make Request", command=self.make_request)
+        self.request_button.grid(columnspan=2, row=6, column=0, padx=10, pady=10)
+        
+        self.output_text = ctk.CTkTextbox(self.window)
+        self.output_text.grid(columnspan=2, row=7, column=0, padx=10, pady=10, sticky="nsew")
+        
+        self.window.mainloop()
+    
+    def on_window_close(self):
+        if self.timer_thread is not None:
+            self.timer_thread.kill()
+        self.window.destroy()
+        exit(0)
+    
+    def fetch_data(self):
+        response = requests.get(self.url, auth=(self.client_id, self.api_key))
+        response_json = response.json()
+        
+        # Check authentication
+        if response.status_code == 401:
             messagebox.showerror("Error", "Invalid client_id or api_key")
             return
-        case 200:
-            # Update the timer label with the value of the timer
-            if timer_thread is not None:
-                timer_thread.kill()
-
-            timer_thread = CountdownThread(
-                response_json["expires_in"], timer_time_label
-            )
-            timer_thread.start()
-
-            output_text.insert(ctk.END, str(response_json) + "\n")
-            output_text.see(ctk.END)
-        case _:
+        elif response.status_code == 200:
+            if self.timer_thread is not None:
+                self.timer_thread.kill()
+            
+            self.timer_thread = CountdownThread(response_json["expires_in"], self.timer_time_label)
+            self.timer_thread.start()
+            
+            self.output_text.insert(ctk.END, str(response_json) + "\n")
+            self.output_text.see(ctk.END)
+        else:
             pass
+        
+        self.request_button.configure(state="normal")
+    
+    def make_request(self):
+        client_id = self.client_id_entry.get()
+        api_key = self.api_key_entry.get()
+        
+        if client_id == "" or api_key == "":
+            messagebox.showerror("Error", "Please provide client_id and api_key")
+            return
+        
+        with open("secret.json", "w") as f:
+            json.dump({"client_id": client_id, "client_secret": api_key}, f)
+        
+        self.request_button.configure(state="disabled")
+        thread = threading.Thread(target=self.fetch_data)
+        thread.start()
 
-    request_button.configure(state="normal")
-
-
-def make_request():
-    client_id = client_id_entry.get()
-    api_key = api_key_entry.get()
-
-    if client_id == "" or api_key == "":
-        messagebox.showerror("Error", "Please provide client_id and api_key")
-        return
-
-    # Since the value of client_id and api_key are now known,
-    # we can write them back to the secret.json file
-    with open("secret.json", "w") as f:
-        json.dump({"client_id": client_id, "client_secret": api_key}, f)
-
-    request_button.configure(state="disabled")
-    thread = threading.Thread(target=fetch_data)
-    thread.start()
-
-
-# Create the GUI window
-window = ctk.CTk()
-window.title("Dynu API Updater")
-window.iconbitmap("favicon.ico")
-window.protocol("WM_DELETE_WINDOW", on_window_close)
-
-window.minsize(400, 700)
-
-window.grid_columnconfigure((0, 1), weight=1)
-window.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=0)
-window.grid_rowconfigure(7, weight=30)
-
-# Create labels and entry fields for client_id and api_key
-client_id_label = ctk.CTkLabel(window, text="Client ID:")
-client_id_label.grid(row=0, column=0)
-client_id_entry = ctk.CTkEntry(window)
-client_id_entry.insert(ctk.END, client_id)
-client_id_entry.grid(row=1, column=0)
-
-
-api_key_label = ctk.CTkLabel(window, text="API Key:")
-api_key_label.grid(row=2, column=0)
-api_key_entry = ctk.CTkEntry(window)
-api_key_entry.insert(ctk.END, api_key)
-api_key_entry.grid(row=3, column=0)
-
-# A timer to show how long you can make API requests for
-timer_label = ctk.CTkLabel(window, text="Timer (seconds):")
-timer_label.grid(row=1, column=1)
-
-timer_time_label = ctk.CTkLabel(window, text="0")
-timer_time_label.grid(row=2, column=1)
-
-# Create a button to make the request
-request_button = ctk.CTkButton(window, text="Make Request", command=make_request)
-request_button.grid(columnspan=2, row=6, column=0, padx=10, pady=10)
-
-# Create a text window to display the output
-output_text = ctk.CTkTextbox(window)
-output_text.grid(columnspan=2, row=7, column=0, padx=10, pady=10, sticky="nsew")
-
-# Start the GUI event loop
-window.mainloop()
+# Create an instance of the DynuAPIUpdater class
+dynu_updater = DynuAPIUpdater()
